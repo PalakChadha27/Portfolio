60540353b2f933e50e39d94be6e2bf635ce7468e
Palak Chadha <chaddapalak551@gmail.com>
2025-01-02 09:46:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings: [" BTech CSE student","Novice","Aspiring Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
